a5c3923993c920f3a072f6eb32e798dc0f4e110c
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-03-18 16:12:53 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# import requests
+#
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
+
+import requests
+from datetime import datetime
+
+MY_LAT = -19.917299
+MY_LONG = -43.934559
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+print(sunrise)
+
+time_now = datetime.now()
+
+print(time_now)
